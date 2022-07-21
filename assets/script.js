60ecf4c9c6c62bc2b80021464dc9d5d97e28bbe7
Harshil Kaneria <36322779+Harshil-Kaneria@users.noreply.github.com>
2022-07-21 11:19:19 +0530

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -14,8 +14,13 @@ let total_access_time_input = document.querySelector("#total_access_time");
 let leave_time = document.querySelector("#leave_time");
 let finish_at = document.querySelector('#finish_at');
 let remaining_leave_time = document.querySelector("#remaining_leave_time");
+let notification_display = document.querySelector("#notification_display");
+let show_notification = false;
 
 function calculate_time(){
+
+	show_notification = false;
+
     break_time.value=="" ? break_time.value = 0 : '';
     work_time_minutes.value=="" ? work_time_minutes.value = 0 : '';
     work_time_hours.value=="" ? work_time_hours.value = 0 : '';
@@ -23,7 +28,7 @@ function calculate_time(){
     final_time = moment(start_time.value,"HH:mm");
     final_time = final_time.add(Number(work_time_hours.value),'hour').add(Number(work_time_minutes.value)+Number(break_time.value),'minutes')
 
-    finish_at.value = final_time.format('h:mm a Do MMMM YYYY');
+    finish_at.value = final_time.format('h:mm a');
 	remaining_time_calculator()
 	total_working_time()
 }
@@ -47,12 +52,17 @@ function calculate_access_time(){
 
 function calculate_remaining_leave_time_set(){
 	var duration = moment.duration(exit_time.diff(moment()));
+	if(Math.trunc(duration.asSeconds()) == 0 && leave_time.value!="" && show_notification==false && notification_display.checked){
+		show_notification = true
+		generate_notification()
+	}
 	if(duration.asSeconds() <= 0) {
-		localStorage.removeItem("time_calculator_leave_time");
-		remaining_leave_time.value = "Time is Over"
-		clearInterval(counter_interval_remaining_leave_time);
+		// localStorage.removeItem("time_calculator_leave_time");
+		// remaining_leave_time.value = "Time is Over"
+		// clearInterval(counter_interval_remaining_leave_time);
+		remaining_leave_time.value =  duration.hours() + ":" + duration.minutes()  + ":" + duration.seconds()
 	}else{
-		remaining_leave_time.value =  duration.hours() + ":" + duration.minutes()  + ":" + duration.seconds() + " and " + duration.days() + " days "
+		remaining_leave_time.value =  duration.hours() + ":" + duration.minutes()  + ":" + duration.seconds()
 	}
 }
 
@@ -67,16 +77,21 @@ function calculate_remaining_leave_time(){
 
 function remaining_time_set(){
 	var duration = moment.duration(final_time.diff(moment()));
+	if(Math.trunc(duration.asSeconds()) == 0 && leave_time.value=="" && show_notification==false && notification_display.checked){
+		show_notification = true
+		generate_notification()
+	}
 	if(duration.asSeconds() <= 0) {
-		localStorage.removeItem("time_calculator");
-		localStorage.removeItem("time_calculator_start_time");
-		localStorage.removeItem("time_calculator_work_time_hours");
-		localStorage.removeItem("time_calculator_work_time_minutes");
-		localStorage.removeItem("time_calculator_break_time");
-		remaining_time.value = "Time is Over"
-		clearInterval(counter_interval);
+		// localStorage.removeItem("time_calculator");
+		// localStorage.removeItem("time_calculator_start_time");
+		// localStorage.removeItem("time_calculator_work_time_hours");
+		// localStorage.removeItem("time_calculator_work_time_minutes");
+		// localStorage.removeItem("time_calculator_break_time");
+		// remaining_time.value = "Time is Over"
+		// clearInterval(counter_interval);
+		remaining_time.value =  duration.hours() + ":" + duration.minutes()  + ":" + duration.seconds()
 	}else{
-		remaining_time.value =  duration.hours() + ":" + duration.minutes()  + ":" + duration.seconds() + " and " + duration.days() + " days "
+		remaining_time.value =  duration.hours() + ":" + duration.minutes()  + ":" + duration.seconds()
 	}
 }
 
@@ -96,12 +111,34 @@ function reset_time(){
 	localStorage.removeItem("time_calculator_work_time_minutes");
 	localStorage.removeItem("time_calculator_break_time");
 	localStorage.removeItem("time_calculator_leave_time");
+	localStorage.removeItem("notification_display");
 	clearInterval(counter_interval);
+	clearInterval(counter_interval_remaining_leave_time);
 	location.reload();
+}
+
+function generate_notification(){
+	if (Notification.permission === "granted") {
+		notification();
+	} else if (Notification.permission !== "denied") {
+		Notification.requestPermission().then(permission => {
+			if(permission === "granted"){
+				notification();
+			}
+		});
+	}  
+}
+
+function notification(){
+	const notification = new Notification("Time Calculator", {
+		body: "Hello There Look Like You Don't See Current Time",
+		icon: "../icon.png"
+	})
 }
 
 document.addEventListener("DOMContentLoaded", function(event) { 
 	leave_time.value = localStorage.getItem("time_calculator_leave_time")
+	notification_display.checked = localStorage.getItem("notification_display")=="false" ? false : true
 	if(localStorage.getItem("time_calculator")){
 		start_time.value = localStorage.getItem("time_calculator_start_time")
 		work_time_hours.value = localStorage.getItem("time_calculator_work_time_hours")
@@ -118,7 +155,12 @@ document.getElementById("calculate_time").onclick = function(){
 	localStorage.setItem("time_calculator_work_time_minutes",work_time_minutes.value)
 	localStorage.setItem("time_calculator_break_time",break_time.value)
 	localStorage.setItem("time_calculator_leave_time",leave_time.value)
+	localStorage.setItem("notification_display",notification_display.checked)
 	calculate_time()
+}
+
+document.getElementById("notification_display").onclick = function(){
+	localStorage.setItem("notification_display",notification_display.checked)
 }
 
 document.getElementById("reset_time").onclick = function(){ 
